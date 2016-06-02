9751a7c42898e158f29964788a13caf44dc3302b
Emanuel Andrade <emanuelgandrade@gmail.com>
2016-06-02 20:34:39 -0300

    teste webservice

---
+++ PerguntasAoProfessor/app/src/main/java/com/emanuelgandrade/perguntasaoprofessor/Greeting.java
@@ -0,0 +1,19 @@
+package com.emanuelgandrade.perguntasaoprofessor;
+
+/**
+ * Created by emanu on 02/06/2016.
+ */
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+}
--- PerguntasAoProfessor/app/src/main/java/com/emanuelgandrade/perguntasaoprofessor/MainActivity.java
+++ PerguntasAoProfessor/app/src/main/java/com/emanuelgandrade/perguntasaoprofessor/MainActivity.java
@@ -3,12 +3,20 @@ package com.emanuelgandrade.perguntasaoprofessor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TESTE MARCIO
+
+        if (savedInstanceState == null) {
+            //getSupportFragmentManager().beginTransaction()
+            //        .add(R.id.container, new PlaceholderFragment())
+            //        .commit();
+        }
     }
 }
